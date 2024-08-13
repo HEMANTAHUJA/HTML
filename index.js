e3c982f74d60fadedba9e22276d6d85e1838f20b
HEMANTAHUJA <ahujahemant449@gmaill.com>
2024-08-13 11:55:42 +0530

    js operator

---
+++ index.js
@@ -0,0 +1,144 @@
+// var num1 = 20;
+// console.log("Hello,techno");
+// alert("Hello techno");
+// var,let,const
+// var firstnum =20;
+// console.log(firstnum);
+// primitive types --> string,number,boolean,undefined,symbol,bigint....
+// reference or object typed data types --> array , object, date,
+
+// var firstvar = "i'm a full stack developer";
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstvar = false;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+
+// var firstvar = undefined;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstvar = undefined;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstarray = [1,2,3,"hello",true,undefined];
+// console.log(firstarray);
+// console.log(typeof firstarray);
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// var firstvar = "Hello techno";
+// firstvar = [20,30];
+// firstvar = {
+//         firstname : "hemant",
+//         lastname : "ahuja"
+// };
+// firstvar = true;
+
+// console.log(firstvar);
+
+
+// var num1 = {
+//      firstname = "hemant";
+// }
+// console.log(num1);
+
+// let num2 = 20;
+
+// var mum1 = 20;
+
+// var num1 = 30;
+//  variable define with var can be redeclared or reassingned 
+
+// let num2 = 40;
+//  num2 = 100;
+// variables defined woth let cannot be redeclared but can be reassigned
+
+// const num3 = 100;
+// num3 = 1000;
+// variables defined with const cannot be redeclared or reassigned
+
+
+// operators --> 
+
+// arithmetic operators
+// + , - , * , /
+
+// let a=10;
+// let b=20;
+
+// let add = a+b;
+// let sub = a-b;
+// let mul = a*b;
+// let div = a/b;
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+
+// let firstName = "hemant";
+// let lastName = "ahuja";
+
+// console.log(firstName+ ""+ lastName );
+
+// console.log(a**b);
+// console.log(a%b);
+// console.log(5+"5")
+// console.log(5-"5")
+// console.log(5*"5")
+// console.log(5/"5")
+
+// addition assignments
+
+// let a = 5;
+// // a=a+10;
+// a += 10;
+
+// console.log(a);
+
+// let str = "hemant";
+// let conStr = +str;
+// console.log( conStr);
+// console.log(typeof conStr); 
+
+// in javascript the typeof notanumber is number
+
+// console.log("shubam" + 10);
+
+// let str1 = "S";
+// let str2 = +str1;
+
+// console.log(S + "S");
+
+// let y =10;
+// y /= 3;
+// console.log(y);
+// mul assignment, div assignment,subtraction assignment (*=,/=,-=)
+
+// comparison operator
+
+let num1 = 10;
+let num2 = "10";
+
+console.log(num1 == num2);
+// in == the datatype is not compared while the === compare the dataype also
+console.log(num1 === num2);
+
+// logical operators
+
+// AND(&&) , OR(||)
+
+let isloggdin = 20;  //1010
+let isadmin = 10;  //10100
+
+console.log("is the user logged in and an admin?",isloggdin && isadmin);
+// in this the lastc value is returned but if any one is 0 then the output will be zero
+
+console.log(10 || 1000);
+
+
