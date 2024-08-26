301945f343cd0094fda623475e3b721f83ced737
HEMANTAHUJA <ahujahemant449@gmaill.com>
2024-08-26 09:47:49 +0530

    Commit done

---
+++ index8.js
@@ -0,0 +1,26 @@
+
+console.log(a);
+console.log(sum());
+
+var a = 20;
+// let a = 20;
+let b = 1000;
+
+// function sum(){
+//     let x = 200;
+//     let y = 300;
+//     return x + y;
+
+// }
+let sum = function(){
+
+        let x = 200;
+    let y = 300;
+    return x + y;
+}
+
+let c = 100;
+
+console.log(a,b,c);
+
+sum();
